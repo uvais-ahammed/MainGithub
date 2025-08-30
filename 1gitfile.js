060d0ccf8fcc0ab39d0abb533328e9cc50c64ae4
ahammed <xyz@gmail.com>
2025-08-30 21:16:12 +0530

    addition

---
+++ 1gitfile.js
@@ -0,0 +1,28 @@
+{
+  "people": [
+    {
+      "id": "1",
+      "firstName": "Joe",
+      "lastName": "Jackson",
+      "gender": "male",
+      "age": 28,
+      "number": "7349282382"
+    },
+    {
+      "id": "2",
+      "firstName": "James",
+      "lastName": "Smith",
+      "gender": "male",
+      "age": 32,
+      "number": "5678568567"
+    },
+    {
+      "id": "3",
+      "firstName": "Emily",
+      "lastName": "Jones",
+      "gender": "female",
+      "age": 24,
+      "number": "456754675"
+    }
+  ]
+}
